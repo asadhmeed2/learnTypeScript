ad2fbb9de2c99c02f2e8ce46afab06a7cd730f0c
newasad <asadhmeed2@gmail.com>
2022-03-02 14:51:16 +0200

    type function arguments

--- app.ts
+++ app.ts
@@ -1,2 +1,14 @@
 const title =document.querySelector('.title') as HTMLElement;
-title.style.color = 'red';
+title.style.color = 'blue';
+
+//ex1 add type to function arguments
+// function add(num1:number,num2:number) {
+//     return num1 + num2;
+// }
+// const number1=5;
+// const number2=6;
+// console.log(add(number1,number2));
+
+//ex2 add type to function arguments
+
+
